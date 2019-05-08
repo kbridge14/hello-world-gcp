1ae53e9130f5f937405ec23e3ca239faec0fc5ac
Kerry Kurcz <kurczkerry@gmail.com>
2019-05-08 18:59:05 -0500

    working flask app

---
+++ main/hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+import sqlalchemy
+import psycopg2
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, Kerry!"
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1", port=5000)
+
