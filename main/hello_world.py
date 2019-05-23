ba72bf2499d49b069b8b7e370d5b125f63eab7ed
Kerry <30440869+kbridge14@users.noreply.github.com>
2019-05-22 23:12:03 -0500

    Update hello_world.py

--- main/hello_world.py
+++ main/hello_world.py
@@ -11,15 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return "Hello, Kerry! u r cool"
-=======
-    return "Hello, Kerry! What is up."
->>>>>>> 214cca2f596b0688ff16ae0ef37198f237780be0
-=======
     return "Hello, Kerry!"
->>>>>>> 53b7cd17c89b2bda8554ac4bf98f55821df74157
 
 
 if __name__ == "__main__":
